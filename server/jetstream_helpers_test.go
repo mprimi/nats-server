d2b255c48c7d99ed24993fc77e60bd0433af6a44
Marco Primi <marco@mpri.me>
2024-10-24 17:37:51 -0700

    Revert "Set sync=always in benchmarks configuration template"
    This reverts commit 506f3b91920ae51680325d00050b7dee935d2a5c.

--- server/jetstream_helpers_test.go
+++ server/jetstream_helpers_test.go
@@ -139,7 +139,7 @@ var jsClusterAccountsTempl = `
 var jsClusterTempl = `
 	listen: 127.0.0.1:-1
 	server_name: %s
-	jetstream: {max_mem_store: 256MB, max_file_store: 2GB, store_dir: '%s', sync_interval: always}
+	jetstream: {max_mem_store: 256MB, max_file_store: 2GB, store_dir: '%s'}
 
 	leaf {
 		listen: 127.0.0.1:-1
